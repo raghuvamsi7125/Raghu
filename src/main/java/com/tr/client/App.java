e3b67fae0bd039f4d117750a261f0122d78e6da0
Raghu <raghuvamsi836@gmail.com>
2023-08-27 12:40:33 +0530

    my first project

---
+++ src/main/java/com/tr/client/App.java
@@ -0,0 +1,13 @@
+package com.tr.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
